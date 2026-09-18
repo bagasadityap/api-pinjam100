932b67dfcd92014b360fec41018911ca54dd76f9
bagasaditya <bagasadit78@gmail.com>
2026-09-18 22:17:28 +0700

    fix: firebase config failed in test

--- src/test/java/com/bagas/pinjam100/Pinjam100ApplicationTests.java
+++ src/test/java/com/bagas/pinjam100/Pinjam100ApplicationTests.java
@@ -1,13 +1,17 @@
 package com.bagas.pinjam100;
 
+import com.bagas.pinjam100.service.notification.FirebaseNotificationService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class Pinjam100ApplicationTests {
 
+    @MockitoBean
+    FirebaseNotificationService firebaseNotificationService;
+
     @Test
     void contextLoads() {
     }
-
 }
