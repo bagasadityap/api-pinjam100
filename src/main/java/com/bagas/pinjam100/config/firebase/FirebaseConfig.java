26cd17cb0418ae7d379951515faa023641696250
bagasaditya <bagasadit78@gmail.com>
2026-09-18 22:06:18 +0700

    fix: backend CI workflow env

--- src/main/java/com/bagas/pinjam100/config/firebase/FirebaseConfig.java
+++ src/main/java/com/bagas/pinjam100/config/firebase/FirebaseConfig.java
@@ -4,10 +4,12 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.io.IOException;
 
 @Configuration
+@Profile("!test")
 public class FirebaseConfig {
 
     public FirebaseConfig() throws IOException {
