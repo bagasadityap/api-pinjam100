c470c6e19ea3e95373f9e7c7a460ad1b90a28ebb
bagasaditya <bagasadit78@gmail.com>
2026-09-18 16:56:08 +0700

    fix: backend CI workflow env

--- src/test/java/com/bagas/pinjam100/Pinjam100ApplicationTests.java
+++ src/test/java/com/bagas/pinjam100/Pinjam100ApplicationTests.java
@@ -2,13 +2,12 @@ package com.bagas.pinjam100;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class Pinjam100ApplicationTests {
 
     @Test
     void contextLoads() {
     }
+
 }
